2ebefd3db8b5b149e3907401dd5b9242ddfa3630
Remzeey <remzwisdom@gmail.com>
2023-09-15 14:18:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className=" w-full ">
+      <div>
+        <p className="text-2xl font-bold top-10 m-7 font-sans">
+          {" "}
+          404 NOT FOUND
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className=" flex flex-wrap mx-auto m-40 font-mono ">
+        <div>
+          <img
+            src="/Scarecrow.png"
+            alt="Scarecrow"
+            height={447.43}
+            width={539.22}
+          />
+        </div>
+        <div className=" m-12">
+          <p className=" font-bold text-5xl ">I have some bad news for you</p>
+          <p className=" font-normal font-spacemono text-2xl w-96 h-28 left-96 ">
+            The page you are looking for might be removed or is temporarily
+            available
+          </p>
+          <button className=" bg-gray-700 text text-white text-sm font-mono font-bold w-40">
+            <p className=" m-3 font-bold font-spacemonon">BACK TO HOMEPAGE</p>
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center text-gray-500">
+        Created by Remzeey - devChallenges.io
+      </div>
     </div>
   );
 }
